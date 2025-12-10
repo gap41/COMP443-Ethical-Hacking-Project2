6a668bcd81ff874a683f9c127484492854f6a3dd
gap41 <william@varholm.no>
2025-12-10 18:04:38 +0200

    Added comments

--- dbSetupScript.c
+++ dbSetupScript.c
@@ -4,7 +4,7 @@
  * 
  * This is the source code to create, populate and query the database
  * 
- * This is a part of the Project 2 submission for the class COMP-443 Ethical Hacking December 2025
+ * This is a part of the Project 3 submission for the class COMP-443 Ethical Hacking December 2025
  * 
  */
 
@@ -20,6 +20,7 @@
 /**********************************
  * Prototypes
  **********************************/
+//Callback is used to print the output of the SELECT query
 static int callback(void *, int argc, char **argv, char **azColName);
 
 /**********************************
@@ -28,22 +29,22 @@ static int callback(void *, int argc, char **argv, char **azColName);
 
 int createDB(){
 
-    sqlite3 *db;
+    sqlite3 *db; // Pointer to the sqlite3 struct, could be thought of as the database object
     char *zErrMsg = 0; //Error message for when executing the sqlite3_execute() function call
-    int rc;
-    char *sql;
+    int rc; // Variable to save the status of the database function commited to check if it was successful or not
+    char *sql; // pointer to point to the sql query string
     const char* data = "Callback was called";
 
-    rc = sqlite3_open("employee.db", &db);
+    rc = sqlite3_open("employee.db", &db); //Open the database, or create it if it doesn't exist
 
-
-    if(rc){
+    if(rc){ //Error handling if the opening of the database failed
         printf("Can't open database: %s\n", sqlite3_errmsg(db));
-        return 0;
+        return -1;
     }else{
         printf("Opened database successfully\n");
     }
 
+    //The query to create the table EMPLOYEES
     sql = "CREATE TABLE EMPLOYEES(" \
         "ID INT PRIMARY KEY     NOT NULL," \
         "NAME           TEXT    NOT NULL," \
@@ -54,36 +55,38 @@ int createDB(){
         "PASSWORD       CHAR(10));"
         ;
 
+    //Execute the SQL statement
     rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
 
-    if(rc != SQLITE_OK){
+    if(rc != SQLITE_OK){    //Error handling if the query fails
         printf("Error executing the SQL query : %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
+        sqlite3_free(zErrMsg);  //Freeing up the memory allocated to the zErrMsg
     }else{
         printf("The table has been created\n\n\n");
     }
 
-    sqlite3_close(db);
+    sqlite3_close(db);  //Close the database connection
 
 }
 
 int populateDB(){
 
-    sqlite3 *db;
+    sqlite3 *db; // Pointer to the sqlite3 struct, could be thought of as the database object
     char *zErrMsg = 0; //Error message for when executing the sqlite3_execute() function call
-    int rc;
-    char *sql;
+    int rc; // Variable to save the status of the database function commited to check if it was successful or not
+    char *sql; // pointer to point to the sql query string
     const char* data = "Callback was called";
 
     rc = sqlite3_open("employee.db", &db);
 
-    if(rc){
+    if(rc){ //Error handling if the opening of the database failed
         printf("Can't open database: %s\n", sqlite3_errmsg(db));
-        return 0;
+        return -1;
     }else{
         printf("Opened database successfully\n");
     }
 
+    //The SQL query to populate the database
     sql = "INSERT INTO EMPLOYEES(ID,NAME,AGE,ADDRESS,SALARY, USERNAME, PASSWORD) "  \
          "VALUES (1, 'Anders', 34, 'Oslo', 540000, 'AND', 'password123' ); " \
          "INSERT INTO EMPLOYEES(ID,NAME,AGE,ADDRESS,SALARY, USERNAME, PASSWORD) "  \
@@ -93,57 +96,59 @@ int populateDB(){
          "INSERT INTO EMPLOYEES(ID,NAME,AGE,ADDRESS,SALARY, USERNAME, PASSWORD) " \
          "VALUES (4, 'Morten', 28, 'Fredrikstad ', 550000, 'MOR', 'Password4Work123' );";
 
-    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg); // Executing the SQL query
 
-    if(rc != SQLITE_OK){
+    if(rc != SQLITE_OK){    //Error handling if the SQL query fails to execute
         printf("Error executing the SQL query : %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
+        sqlite3_free(zErrMsg); //Freeing up the memory allocated to the zErrMsg
     }else{
         printf("The data has been added\n\n\n");
     }
 
-    sqlite3_close(db);
+    sqlite3_close(db);  //Closes the database connection
 
     return 0;
 }
 
 int queryDB(){
 
-    sqlite3 *db;
+    sqlite3 *db; // Pointer to the sqlite3 struct, could be thought of as the database object
     char *zErrMsg = 0; //Error message for when executing the sqlite3_execute() function call
-    int rc;
-    char *sql;
+    int rc; // Variable to save the status of the database function commited to check if it was successful or not
+    char *sql; // pointer to point to the sql query string
     const char* data = "Callback was called";
 
-    char query[100];
+    char query[100];    // Creating a character array fitting 100 characters for the SQL query
 
     rc = sqlite3_open("employee.db", &db);
 
-    if(rc){
+    if(rc){     //Error handling if the opening of the database failed
         printf("Can't open database: %s\n", sqlite3_errmsg(db));
-        return 0;
+        return -1;
     }else{
         printf("Opened database successfully\n");
     }
 
-    char buff[100];
+    char buff[100];     // Creating a character array fitting 100 characters for the user input
 
-    strcpy(query, "SELECT NAME, ADDRESS FROM EMPLOYEES WHERE NAME LIKE '");
+    strcpy(query, "SELECT NAME, ADDRESS FROM EMPLOYEES WHERE NAME LIKE '"); // Copy the provided string into the query character array
 
     printf("Name: ");
 
-    fgets(buff, 70, stdin);
+    fgets(buff, 70, stdin);     // Stores the user input into the buff character array, restricts the length of the user input to avoid buffer overflow
 
-    //Clear stdin cache after so that fgets doesn't read the \n from the stdin cache next time it is called, also has to check if \n was read into buff or is still in the stdin cache
+    // Clear stdin cache after so that fgets doesn't read the \n from the stdin cache next time it is called, also has to check if \n was read into buff or is still in the stdin cache
     if(buff[strlen(buff)-1] != '\n'){
         int c;
         while ((c = getchar()) != '\n' && c != EOF) { }
     }
 
+    // Removes the \n index from the character array from the user input to prepare to concatenate the user input into the SQL query without the newline character
     if(buff[strlen(buff)-1] == '\n'){
         buff[strlen(buff)-1] = '\0';
     }
 
+    //Concatenates the user input to the SQL query and then adds the apostrophe at the end
     strcat(query, buff);
     strcat(query, "'");
 
@@ -155,20 +160,22 @@ int queryDB(){
 
     rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
 
-    if(rc != SQLITE_OK){
+    if(rc != SQLITE_OK){    // Error handling if the query fails to execute
         printf("Error executing the SQL query : %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
+        sqlite3_free(zErrMsg); //Freeing up the memory allocated to the zErrMsg
     }else{
         printf("Query successfully executed\n\n\n");
     }
 
-    sqlite3_close(db);
+    sqlite3_close(db);  // Close the database connection
 
     return 0;
 
 }
 
 static int callback(void *, int argc, char **argv, char **azColName){
+    // Goes through each argument by using the argument counter
+    // Each argument is the result of the SQL Query, e.g. SELECT
     for(int i = 0; i < argc; i++){
         if(argv[i] != NULL){
             printf("%s = %s\n", azColName[i], argv[i]);
@@ -184,10 +191,11 @@ static int callback(void *, int argc, char **argv, char **azColName){
 
 int dropDB(){
     
-    if(remove("employee.db") == 0){
+    if(remove("employee.db") == 0){ // If the database exist delete the file
         printf("Successfully deleted the database file\n\n\n");
     }else{
-        perror("Error deleting the database");
+        perror("Error deleting the database");  // Print error if error occurs
+        return -1;
     }
 
     return 0;
--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  * 
  * This is the main project file with the tool to connect to the database
  * 
- * This is a part of the Project 2 submission for the class COMP-443 Ethical Hacking December 2025
+ * This is a part of the Project 3 submission for the class COMP-443 Ethical Hacking December 2025
  * 
  */
 
@@ -27,10 +27,11 @@ int chooseAction();
 int queryTheDB();
 
 int main(){
-    int action = 0;
-    bool isDone = false;
 
-    do{
+    int action = 0; // Store the input of the user
+    bool isDone = false;    // Turns to true if the user chooses action 5 to exit the program
+
+    do{     // Do-while looping until user wishes to exit the program
         action = chooseAction();
         switch (action)
         {
@@ -51,7 +52,7 @@ int main(){
             isDone = true;
             break;
         default:
-            printf("Error action is invalid: %d", action);
+            printf("Error action is invalid: %d", action);  // Error clause if the chooseAction() function returns an invalid number
             return -1;
         }
     }while(!isDone);
@@ -63,17 +64,16 @@ int chooseAction(){
 
     int action = 0;
 
-    //Buffer for the input of the action
-    char buff[2];
+    char buff[2]; // Buffer for the input of the action
     printf("Welcome to the employee database query tool, choose your action:\n\n");
 
     while(!((action > 0) && (action < 6))){
         printf("1. Create the database for the employees\n2. Populate the database for the employees\n3. Search the address for an employee\n4. Drop the database\n5. Exit the program\n\n");
         printf("Type the number corresponding to the action you want to execute: ");
 
-        fgets(buff, 2, stdin);
+        fgets(buff, 2, stdin);  // Store the user input to buff, limit the user input to two characters
 
-        action = atoi(buff);
+        action = atoi(buff);    // Converts the string to an integer
         
         //Clear stdin cache after so that fgets doesn't read the \n from the stdin cache next time it is called, also has to check if \n was read into buff or is still in the stdin cache
         if(buff[strlen(buff)-1] != '\n'){
@@ -81,6 +81,7 @@ int chooseAction(){
             while ((c = getchar()) != '\n' && c != EOF) { }
         }
         
+        // Let user know if it is not a valid choice
         if(!((action > 0) && (action < 6))){
             printf("\n%d is not a valid action\n", action);
             printf("\n---------------------\n\nPlease enter a valid action \n\n");
